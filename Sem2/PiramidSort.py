c16c3b267783b091c070d4b0bae35eb0b835b286
BorisLaskin <varislaskin@list.ru>
2023-09-12 15:10:20 +0300

    Sem 2 done

--- Sem2/PiramidSort.py
+++ Sem2/PiramidSort.py
@@ -1,9 +1,10 @@
 from random import randint
 
-list_rand_int = [randint(0, 100) for _ in range(20)]
+list_rand_int = [randint(0, 100) for _ in range(100)]
 
 print(*list_rand_int)
 
+# Описание процеруды просеивания
 def heapifyPiramid(array, size, root):
     largest = root
     leftChild = 2 * root + 1
@@ -17,18 +18,20 @@ def heapifyPiramid(array, size, root):
         array[largest], array[root] = array[root], array[largest]
         heapifyPiramid(array, size, largest)
 
+#
 def piramidSort(array):
+    # Составляем максимальную кучу
     N = len(array)
     for i in range(int(N / 2) - 1, -1, -1):
-        heapifyPiramid(array, N - i, i)
+        heapifyPiramid(array, N, i)
 
     # print(*array)
-
-    for i in range(N -1,0,-1):
+    # начинаем вытаскивать максимальные элементы по одному и помещаем их в конец в лексикографическом порядке умменьшения
+    for i in range(N - 1, 0, -1):
         array[i], array[0] = array[0], array[i]
         heapifyPiramid(array, i, 0)
         # print(*array)
-        
+
 
 piramidSort(list_rand_int)
 print(*list_rand_int)
